fc9e309a32b33032e2f380ec9623e95dae07d169
David Martin <d.r.martin.wrk@gmail.com>
2023-02-08 16:57:02 -0500

    alot

--- app.js
+++ app.js
@@ -2,22 +2,21 @@
 
  
 const cards = document.querySelectorAll('.card');
-// const card2 = document.querySelectorAll('.card');
-// const card3 = document.querySelectorAll('.card');
-// const card4 = document.querySelectorAll('.card');
-// const card5 = document.querySelectorAll('.card');
-// const card6 = document.querySelectorAll('.card');
-// const card7 = document.querySelectorAll('.card');
-// const card8 = document.querySelectorAll('.card');
-// const card9 = document.querySelectorAll('.card');
-// const card10 = document.querySelectorAll('.card');
-// const card11 = document.querySelectorAll('.card');
-// const card12 = document.querySelectorAll('.card');
-// const card13 = document.querySelectorAll('.card');
-// const card14 = document.querySelectorAll('.card');
-// const card15 = document.querySelectorAll('.card');
-// const card16 = document.querySelectorAll('.card');
 
+
+let colorArray = ["red", "red", "blue", "blue", "greenyellow",  "greenyellow", "orange", "orange", "yellow", "yellow","brown", "brown","black", "black", "fuchsia", "fuchsia"] //style for 16 colors
+
+//shuffle color array
+
+init();
+function init() {
+//    for ( let i = 0; i < 16; i++) {
+//        cards[i].dataset.color = colorArray[i];
+//     }
+cards.forEach((card, i) => {
+    card.dataset.color = colorArray[i];
+})
+   }
 
 //card array
 
@@ -25,8 +24,6 @@ const cards = document.querySelectorAll('.card');
 //                  card5, card6, card7, card8, 
 //                  card9, card10, card11, card12, 
 //                  card13, card14, card15, card16];
- let cardIsFlipped = false;
- let cardOne, cardTwo;
 
 //stored variables
 
@@ -34,20 +31,67 @@ const cards = document.querySelectorAll('.card');
 // const startBtn = document.getElementById("start"); const replayBtn = document.getElementById("replay");
 
 
-function cardFlip() {
-    console.log('click!', cards);
-    // console.log(this);
-    this.classList.add('card-front');
-    if (cardFlip) {
-        cardFlip = true;
+//start button / timer
+let cardIsFlipped = false;
+let cardOne, cardTwo;
+
+let counter = 0;
+//card=flip
+function cardFlip(evt) {
+    // console.log('click!', cards);
+    console.log(evt.target);
+    let cardColor = evt.target.dataset.color;
+    console.log(cardColor)
+    evt.target.classList.toggle(cardColor);
+    console.log(evt.target);
+    // this.classList.toggle('card-front');
+    console.log(counter)
+    if (counter === 0) {
+        cardIsFlipped = false; counter++;// counter +1 on click event
         cardOne = this;
+        console.log(counter);
     } else {
-        cardFlip = false;
+        cardIsFlipped = false;
         cardTwo = this;
-    
-       console.log(cardOne, cardTwo); 
+        counter = 0
+        console.log(cardOne, cardTwo); 
+        if (cardOne.dataset.color === cardTwo.dataset.color) {
+            console.log('match');
+        }
     }
 }
 
 cards.forEach(card => card.addEventListener('click', cardFlip));
 
+//Game logic / point log
+
+//start button
+    //activate board, begin count
+
+
+//timer
+//msg: "Time's Up!"
+let timer;
+let timeLeft = 60; //seconds (timer function in milliseconds)
+
+
+function gameOver() {
+    cancelInterval(timer);
+}
+
+function updateTimer() {
+    timeLeft = timeLeft - 1;
+    if(timeLeft >= 0)
+        $('#timer').html(timeLeft);
+        else {
+            gameOver();
+        }
+}
+
+
+
+
+
+//shuffle
+
+//reset
